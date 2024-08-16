ab24fcfa6fbe48ec2aac43fd3378a1b0ad0b9b3c
dhdenchev <139794907+dhdenchev@users.noreply.github.com>
2024-08-16 11:30:41 +0100

    m

--- indexv2.0.js
+++ indexv2.0.js
@@ -578,6 +578,16 @@ document.addEventListener("DOMContentLoaded", function () {
               "UW_Observation_Should_We_Buy_Net_Profit"
             ).textContent = resultObj.creatorRecord[0].Net_Profit || "-";
             //
+            //Profit Share Details
+            document.getElementById(
+              "UW_Observation_Should_We_Buy_Profit_Share_Details"
+            ).textContent = resultObj.creatorRecord[0].Net_Profit || "-";
+            //
+            //Disposal Strategy
+            document.getElementById(
+              "UW_Observation_Should_We_Buy_Disposal_Strategy"
+            ).textContent = resultObj.creatorRecord[0].Net_Profit || "-";
+            //
             //------------------------------------------------------------------------------------
             //Comments and Stars ratings data
             //CONDITION -------------------------------------------------------------------------------------
