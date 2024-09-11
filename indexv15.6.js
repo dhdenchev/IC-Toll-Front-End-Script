ff65e2b0ff7355673c27fd10ccc8e9be6cd34e38
dhdenchev <139794907+dhdenchev@users.noreply.github.com>
2024-09-11 11:07:12 +0100

    change env

--- indexv15.6.js
+++ indexv15.6.js
@@ -246,7 +246,7 @@ const saveObservationRating = (sOppId, fieldToUpdate, valueOfFieldToUpdate) => {
   const productionApiUpdate =
     " https://ic-tool-middleware-b0d5a7c7355b.herokuapp.com/api/updateRecordDetails";
 
-  fetch(productionApiUpdate, {
+  fetch(developmentApiUpdate, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
@@ -285,7 +285,7 @@ const saveValueToCRM = (
   const productionApiUpdateCrm =
     " https://ic-tool-middleware-b0d5a7c7355b.herokuapp.com/api/updateCrmRecordDetails";
 
-  fetch(productionApiUpdateCrm, {
+  fetch(developmentApiUpdateCrm, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
@@ -716,7 +716,7 @@ const productionApiGet =
 
 document.addEventListener("DOMContentLoaded", function () {
   if (sOppId) {
-    fetch(productionApiGet, {
+    fetch(developmentApiGet, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
