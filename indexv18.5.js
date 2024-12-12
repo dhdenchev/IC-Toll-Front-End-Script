67e3beca8e3aa9abf53197462f6ebeb4b4783824
dhdenchev <139794907+dhdenchev@users.noreply.github.com>
2024-12-12 22:48:43 +0000

    make the sync between crm-creator-ictool

--- indexv18.5.js
+++ indexv18.5.js
@@ -854,9 +854,9 @@ document.addEventListener("DOMContentLoaded", function () {
             document.getElementById("Upstix_Assessed_Value").textContent =
               "£" + formatNumber(resultObj.uwValuation) || "-";
             document.getElementById("IC1_Proposed_Offer").textContent =
-              "£" + formatNumber(resultObj.uwOffer) || "-";
+              "£" + formatNumber(resultObj.uwOfferLower) || "-";
             document.getElementById("Proposed_Offer_Upper").textContent =
-              "£" + formatNumber(resultObj.uwOffer) || "-";
+              "£" + formatNumber(resultObj.uwOfferUpper) || "-";
             document.getElementById("180DayValuation").textContent =
               "£" + formatNumber(resultObj["180DayValuation"]) || "-";
 
